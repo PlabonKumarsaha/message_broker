a1ea0befefeca5dcdd5b3ed67dd52b4c9e4f8860
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-07-08 11:43:14 +0600

    Add files via upload

---
+++ spring-abbit-mq-produccer/src/main/java/com/ERA/rabbitmq/MessagePublisher.java
@@ -0,0 +1,24 @@
+package com.ERA.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+public class MessagePublisher {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @PostMapping("/publish")
+    public String publishMessage(@RequestBody CustomMessage message){
+    message.setMessage(UUID.randomUUID().toString());
+    message.setMessageDate(new Date());
+    rabbitTemplate.convertAndSend(MQ_config.EXCHANGE,MQ_config.ROUTING_KEY,message);
+    return "message published";
+    }
+}
